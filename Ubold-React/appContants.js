2d38f01be479ade7d97c04fc08cf82a73bed8a7d
Mohammed Rizwan <rizwan@truscholar.io>
2024-12-09 13:06:39 +0530

    Rizwan | add : authentication endpoint

---
+++ Ubold-React/appContants.js
@@ -0,0 +1,8 @@
+
+const HOST = "http://localhost:8080";
+
+
+// Auth
+const AUTHENTICATION = "/api/auth"
+export const USER_SIGNUP = `${AUTHENTICATION}/signup`;
+export const USER_LOGIN = `${AUTHENTICATION}/signin`;
